f174781a59a7913dbf3f416e586ef42edaa4a190
Dennis Patzer <info@straightsoft.de>
2017-05-07 15:07:50 +0200

    see cl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.0.3] - 2017-05-07
+
+### Added
+- keyExists()
+
 ## [1.0.2] - 2017-05-07
 
 ### Fixed
--- classes/DatabaseCache.php
+++ classes/DatabaseCache.php
@@ -8,6 +8,12 @@ class DatabaseCache
 {
     const DEFAULT_MAX_CACHE_TIME = ['unit' => 'd', 'value' => 1];
 
+    public static function keyExists($strKey)
+    {
+        $objResult = \Database::getInstance()->prepare('SELECT * FROM tl_db_cache WHERE cacheKey = ?')->execute($strKey);
+        return $objResult->numRows > 0;
+    }
+
     public static function getValue($strKey)
     {
         if (!\Config::get('activateDbCache'))
@@ -32,6 +38,11 @@ class DatabaseCache
 
     public static function cacheValue($strKey, $strValue)
     {
+        if (!\Config::get('activateDbCache'))
+        {
+            return false;
+        }
+
         if (static::getValue($strKey))
         {
             throw new \Exception('Duplicate entry in tl_db_cache for key ' . $strKey);
